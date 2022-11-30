4c439d475e97420552b99b2707ff2e5f30e13f7f
ieie727 <103009461+ieie727@users.noreply.github.com>
2022-11-30 10:57:11 +0900

    Update game.js

--- monohaji_202202/game.js
+++ monohaji_202202/game.js
@@ -6,27 +6,25 @@ const h = canvas.height;
 
 
 //答えとなる猫の数の設定とカウントの準備
-const result = getRandomNumber(1,5);
+const result = 2;
 let count = 0;
 
-
-//動物の設定（X座標、Y座標、種類）
+//動物の画像の読み込みや設定（種類、X座標、Y座標）
 let animal = new Image();
-let animalType = setAnimalType();
+animal.src = "image/cat.png";
+let animalType = "cat";
 let animalX = getRandomNumber(500,w);
 let animalY = getRandomNumber(0,h);
 
 
 //こたつの画像を用意
-const kotatsu = new Image();
-kotatsu.src = "image/kotatsu.png";
 
 
-//スタート・再スタートボタン
+//スタート・リスタートボタン
 const startButton = document.getElementById('start');
 let id;
 let flag = false;
-kotatsu.onload = () => ctx.drawImage(kotatsu, 0, 200, 400, 200);
+//kotatsu.onload = () => ctx.drawImage(kotatsu, 0, 200, 400, 200);
 startButton.addEventListener('click', () => {
     if(flag === false){
         //ゲーム開始の処理
@@ -67,16 +65,19 @@ function draw(){
 
         //猫の数が正解数に達するまで出現させて、答えまで達すると答え合わせの処理をする
         if(count < result){
-            //動物の位置・種類の再設定
-            animalType = setAnimalType();
+            //動物の画像の読み込みや設定をし直す
+            animal = new Image();
+            animal.src = "image/cat.png";
+            animalType = "cat";
             animalX = getRandomNumber(500,w);
             animalY = getRandomNumber(0,h);
         }else{
-            //正解発表をしてゲームを終了する
-            showCorrectAnswer();
+            //答え合わせをして、ゲームを終了する
             clearInterval(id);
             return;
         }
+            
+        
     }
 }
 
@@ -137,9 +138,6 @@ function showCorrectAnswer(){
     }
     
     //正解数の猫を表示
-    let showAnimalX = 200;
-    for(let i=0; i<count; i++){
-        showAnimalX = showAnimalX + 60;
-        ctx.drawImage(animal, showAnimalX, 250, 60, 60);
-    }
+    
+    
 }
